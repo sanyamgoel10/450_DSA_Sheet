0ff071e668e637c4574e27344270eca2efa6ce92
sanyamgoel10 <sanyamgoel10@gmail.com>
2023-02-21 22:20:05 +0530

    -

--- Move_all_the_negative_elements_to_one_side_of_the_array.cpp
+++ Move_all_the_negative_elements_to_one_side_of_the_array.cpp
@@ -21,6 +21,7 @@ void move(vector<int>& nums){
         }
     }
 }
+
 int main() {
     vector<int> arr = { -1, 2, -3, 4, 5, 6, -7, 8, 9 };
     move(arr);
--- reverse_an_array_or_string.cpp
+++ reverse_an_array_or_string.cpp
@@ -1,10 +1,16 @@
 // Write a program to reverse an array or string
 
-void revereseArray(vector<int>& arr, int l, int r){
+void reverseArray1(vector<int>& arr, int l, int r){
     while(l<r){
         swap(arr[l], arr[r]);
         l++;
         r--;
+    }
+}
+
+void reverseArray2(vector<int>& arr, int n){
+    for(int i=0;i<arr.size()/2;i++){
+        swap(arr[i], arr[n-i-1]);
     }
 }
 int main()
@@ -16,13 +22,14 @@ int main()
         cout<<x<<" ";
     }
     
-    revereseArray(arr, 0, n-1);
+    reverseArray1(arr, 0, n-1);
     
     cout<<endl;
     cout<<"Resultant array: ";
     for(auto x : arr){
         cout<<x<<" ";
     }
-     
+    
+    reverseArray2(arr, n);
     return 0;
 }
